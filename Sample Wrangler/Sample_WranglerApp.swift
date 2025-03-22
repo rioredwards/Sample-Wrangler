96864842b17b8533996baf83d092ddcce8e9c832
Rio Edwards <rioredwards@gmail.com>
2025-03-22 13:32:56 -0700

    Initial Commit

---
+++ Sample Wrangler/Sample_WranglerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample_WranglerApp.swift
+//  Sample Wrangler
+//
+//  Created by Rio Edwards on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct Sample_WranglerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
